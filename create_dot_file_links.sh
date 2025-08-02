893c53d654a44ec94f4dde57af5af2c124060e1c
Tanner Weber <tannerw@cecs.pdx.edu>
2025-08-02 00:33:47 -0700

    Updated for nvim

--- create_dot_file_links.sh
+++ create_dot_file_links.sh
@@ -1,13 +1,27 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
 # Tanner Weber
 # July 29, 2025
 
-ln -s ~/dot-files/.bash_profile	~/.bash_profile
-ln -s ~/dot-files/.bashrc	~/.bashrc
-ln -s ~/dot-files/.tmux.conf	~/.tmux.conf
-ln -s ~/dot-files/.vimrc	~/.vimrc
-ln -s ~/dot-files/.zshrc	~/.zshrc
-ln -s ~/dot-files/.zsh_profile	~/.zsh_profile
+# bash
+ln -s ~/dot-files/.bash_profile		~/.bash_profile
+ln -s ~/dot-files/.bashrc		~/.bashrc
+
+# zsh
+ln -s ~/dot-files/.zshrc		~/.zshrc
+ln -s ~/dot-files/.zsh_profile		~/.zsh_profile
+
+# tmux
+ln -s ~/dot-files/.tmux.conf		~/.tmux.conf
+
+# vim
+ln -s ~/dot-files/.vimrc		~/.vimrc
+
+# nvim, neovim, lazy
+ln -s ~/dot-files/lua/config/autocmds.lua	~/.config/nvim/lua/config/autocmds.lua
+ln -s ~/dot-files/lua/config/keymaps.lua	~/.config/nvim/lua/config/keymaps.lua
+ln -s ~/dot-files/lua/config/lazy.lua		~/.config/nvim/lua/config/lazy.lua
+ln -s ~/dot-files/lua/config/options.lua	~/.config/nvim/lua/config/options.lua
+ln -s ~/dot-files/lua/plugins/disabled.lua	~/.config/nvim/lua/plugins/disabled.lua
 
 echo "Done linking"
