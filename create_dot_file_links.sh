c3be28857a2b61905e07dee115103a2553acf375
Tanner Weber <tannerw@pdx.edu>
2025-09-04 12:18:16 -0700

    Updated starship

--- create_dot_file_links.sh
+++ create_dot_file_links.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 # Tanner Weber
-# July 29, 2025
+# September 3, 2025
+
+FILE="~/.dotfiles" # Where the cloned git repo is
+
+# fish
+ln -s ~/.dotfiles/fish/config.fish	~/.config/fish/config.fish
+ln -s ~/.dotfiles/fish/fish_variables	~/.config/fish/fish_variables
 
 # bash
 ln -s ~/.dotfiles/.bash_profile		~/.bash_profile
@@ -10,6 +16,9 @@ ln -s ~/.dotfiles/.bashrc		~/.bashrc
 # zsh
 ln -s ~/.dotfiles/.zshrc		~/.zshrc
 ln -s ~/.dotfiles/.zsh_profile		~/.zsh_profile
+
+# starship
+ln -s ~/.dotfiles/starship.toml		~/.config/starship.toml
 
 # tmux
 ln -s ~/.dotfiles/.tmux.conf		~/.tmux.conf
