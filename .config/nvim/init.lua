fb5fed526101090d99a8525d31ca41c5366bbd8a
Tanner Weber <tannerw@pdx.edu>
2025-10-11 20:40:07 -0700

    Change: nvim tabstop to 4

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -27,8 +27,8 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "	-- Space leader
 vim.g.maplocalleader = "\\"
 
-vim.o.tabstop = 8
-vim.o.shiftwidth = 8
+vim.o.tabstop = 4
+vim.o.shiftwidth = 2
 vim.o.expandtab = false
 
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
