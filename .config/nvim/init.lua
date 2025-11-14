7f9fae5471af659cb8db2a2f37491771f2706134
Tanner Weber <tannerw@pdx.edu>
2025-11-13 22:03:44 -0800

    lazydev working

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -156,7 +156,14 @@ require("lazy").setup({
         },
         {
           "folke/lazydev.nvim",
-          --ft = "lua",
+          ft = "lua", -- only load on lua files
+          opts = {
+            library = {
+              -- See the configuration section for more details
+              -- Load luvit types when the `vim.uv` word is found
+              { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            },
+          },
         },
       },
       diagnostics = {
