2fd3156996a6e45d99c848c320fcc52e8465d77a
Tanner Weber <tannerw@pdx.edu>
2025-12-12 23:35:00 -0800

    Added ansi codes for wezterm enter and backspace

--- .wezterm.lua
+++ .wezterm.lua
@@ -155,6 +155,8 @@ config.keys = {
   { key = 'DownArrow', action = act.SendString '\x1b[B' },
   { key = 'RightArrow', action = act.SendString '\x1b[C' },
   { key = 'LeftArrow', action = act.SendString '\x1b[D' },
+  { key = 'Enter', action = act.SendString '\x0A[D' },
+  { key = 'Backspace', action = act.SendString '\x08[D' },
 }
 
 -- Tab bar style
