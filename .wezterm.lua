90d11afbdd040b5b2f1e72df28ec8117d7d5516f
Tanner Weber <tannerw@pdx.edu>
2025-12-13 20:04:16 -0800

    Added custom wezterm tab bar

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,6 +4,11 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+-- Colors
+--local col_mantle = '#181825'
+local col_mantle = 'rgba(0, 0, 0, 0.90)'
+local col_crust = '#11111b'
+
 -- Domains
 config.ssh_domains = {
   {
@@ -37,15 +42,31 @@ config.window_background_opacity = 0.90
 config.font_size = 12
 config.font = wezterm.font 'Hack Nerd Font'
 config.window_decorations = 'RESIZE'
-config.use_fancy_tab_bar = true
+config.tab_bar_at_bottom = false
+config.use_fancy_tab_bar = false
 config.enable_tab_bar = true
+config.tab_max_width = 32
 config.scrollback_lines = 5000
+config.enable_scroll_bar = false
+config.colors = {
+  tab_bar = {
+    background = col_mantle,
+    new_tab = {
+      bg_color = col_mantle,
+      fg_color = 'white',
+    },
+    new_tab_hover = {
+      bg_color = col_mantle,
+      fg_color = 'white',
+      intensity = 'Bold',
+    },
+  },
+}
 
 -- Keyboard Encoding
 config.allow_win32_input_mode = false
 config.enable_kitty_keyboard = true
 
---[[
 -- Start in fullscreen
 local mux = wezterm.mux
 wezterm.on('gui-startup', function(window)
@@ -53,7 +74,6 @@ wezterm.on('gui-startup', function(window)
   local gui_window = window:gui_window();
   gui_window:perform_action(wezterm.action.ToggleFullScreen, pane)
 end)
---]]
 
 -- Binds
 local act = wezterm.action
@@ -73,12 +93,12 @@ config.keys = {
       action = wezterm.action_callback(
         function(window, pane, line)
           if line then
-	    mux.rename_workspace(
-	      window:mux_window():get_workspace(),
-	      line
-	    )
-	  end
-	end
+            mux.rename_workspace(
+              window:mux_window():get_workspace(),
+              line
+            )
+          end
+        end
       ),
     },
   },
@@ -90,6 +110,7 @@ config.keys = {
   { key = 'f', mods = 'ALT', action = act.TogglePaneZoomState },
   { key = '%', mods = 'ALT|SHIFT', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
   { key = '"', mods = 'ALT|SHIFT', action = act.SplitVertical { domain = 'CurrentPaneDomain' }, },
+  { key = 'p', mods = 'ALT', action = act.ActivateCommandPalette },
 
   { key = 'h', mods = 'ALT', action = act{ActivatePaneDirection="Left"} },
   { key = 'j', mods = 'ALT', action = act{ActivatePaneDirection="Down"} },
@@ -159,29 +180,148 @@ config.keys = {
 }
 
 -- Tab bar style
+function remove_exe(text)
+  if text:sub(-4) == '.exe' then
+    text = wezterm.truncate_right(text, #text - 4)
+  end
+
+  return text
+end
+
 function tab_title(tab_info)
   local title = tab_info.tab_title
   -- if the tab title is explicitly set, take that
   if title and #title > 0 then
-    return title
+    title = tab_info.tab_title
+  else
+    -- Otherwise, use the title from the active pane in that tab
+    title = tab_info.active_pane.title
   end
-  -- Otherwise, use the title from the active pane in that tab
-  return tab_info.active_pane.title
+
+  return remove_exe(title)
 end
 
 wezterm.on(
   'format-tab-title',
   function(tab, tabs, panes, config, hover, max_width)
-    local title = tab_title(tab)
+    local title = wezterm.truncate_right(tab_title(tab), max_width - 7)
     if tab.is_active then
       return {
-        { Foreground = { Color = 'white' } },
-        { Background = { Color = 'black' } },
+        { Foreground = { Color = '#303446' } },
+        { Background = { Color = col_mantle } },
+        { Text = ' ' },
+        { Foreground = { Color = '#00ff00' } },
+        { Background = { Color = '#303446' } },
         { Text = ' ' .. title .. ' ' },
+        { Foreground = { Color = 'white' } },
+        { Background = { Color = '#8caaee' } },
+        { Text = ' ' .. tab.tab_index + 1 },
+        { Foreground = { Color = '#8caaee' } },
+        { Background = { Color = col_mantle } },
+        { Text = '' },
+      }
+    else
+      return {
+        { Foreground = { Color = '#444444' } },
+        { Background = { Color = col_mantle } },
+        { Text = ' ' },
+        { Foreground = { Color = '#white' } },
+        { Background = { Color = '#444444' } },
+        { Text = ' ' .. title .. ' ' },
+        { Foreground = { Color = 'black' } },
+        { Background = { Color = '#f2d5cf' } },
+        { Text = ' ' .. tab.tab_index + 1 },
+        { Foreground = { Color = '#f2d5cf' } },
+        { Background = { Color = col_mantle } },
+        { Text = '' },
       }
     end
+    return title
   end
 )
+
+wezterm.on('update-status', function(window, pane)
+  local workspace = window:active_workspace()
+  window:set_left_status(wezterm.format {
+    -- Workspace Name
+    { Foreground = { Color = '#fab387' } },
+    { Background = { Color = col_mantle } },
+    { Text = ' ' },
+    { Foreground = { Color = 'black' } },
+    { Background = { Color = '#fab387' } },
+    { Text = ' ' },
+    { Foreground = { Color = 'white' } },
+    { Background = { Color = col_crust } },
+    { Text = ' ' .. workspace .. ' ' },
+    { Foreground = { Color = col_crust } },
+    { Background = { Color = col_mantle } },
+    { Text = ' ' },
+  })
+end)
+
+function basename(path)
+  path = path:gsub('/+$', '')
+  local name = path:match("([^/]+)$")
+  name = name .. '/'
+  return name or ''
+end
+
+function get_cwd(pane)
+  local cwd_uri = pane:get_current_working_dir()
+  local cwd = 'undefined'
+  if cwd_uri then
+      cwd = cwd_uri.file_path
+  end
+  cwd = basename(cwd)
+  return cwd
+end
+
+wezterm.on('update-right-status', function(window, pane)
+  local date = wezterm.strftime '%a %b %-d %I:%M %p'
+  local hostname = wezterm.hostname()
+  local cwd = get_cwd(pane)
+  window:set_right_status(wezterm.format {
+    -- Current Working Directory
+    { Foreground = { Color = '#f9e2af' } },
+    { Background = { Color = col_mantle } },
+    { Text = '' },
+    { Foreground = { Color = 'black' } },
+    { Background = { Color = '#f9e2af' } },
+    { Text = ' ' },
+    { Foreground = { Color = 'white' } },
+    { Background = { Color = col_crust } },
+    { Text = ' ' .. cwd .. ' ' },
+    { Foreground = { Color = col_crust } },
+    { Background = { Color = col_mantle } },
+    { Text = ' ' },
+    -- Time
+    { Foreground = { Color = '#a6e3a1' } },
+    { Background = { Color = col_mantle } },
+    { Text = '' },
+    { Foreground = { Color = 'black' } },
+    { Background = { Color = '#a6e3a1' } },
+    { Text = '󰃰 ' },
+    { Foreground = { Color = 'white' } },
+    { Background = { Color = col_crust } },
+    { Text = ' ' .. date .. ' ' },
+    { Foreground = { Color = col_crust } },
+    { Background = { Color = col_mantle } },
+    { Text = ' ' },
+    -- Hostname
+    { Foreground = { Color = '#b4befe' } },
+    { Background = { Color = col_mantle } },
+    { Text = '' },
+    { Foreground = { Color = 'black' } },
+    { Background = { Color = '#b4befe' } },
+    { Text = '󰪫 ' },
+    { Foreground = { Color = 'white' } },
+    { Background = { Color = col_crust } },
+    { Text = ' ' .. hostname .. ' ' },
+    { Foreground = { Color = col_crust } },
+    { Background = { Color = col_mantle } },
+    { Text = ' ' },
+  })
+end)
 
 -- Return the configuration to wezterm
 return config
