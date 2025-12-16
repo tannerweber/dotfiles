6bf9b684e925dc414576680ae501a6e7e3c5b215
Tanner Weber <tannerw@pdx.edu>
2025-12-15 19:54:27 -0800

    updated using pack nvim

--- .config/nvim/init_using_pack.lua
+++ .config/nvim/init_using_pack.lua
@@ -2,185 +2,272 @@
 -- ~/.config/nvim/init.lua
 
 vim.pack.add({
-  { src = "https://github.com/folke/tokyonight.nvim.git", name = "tokyonight" },
-  { src = "https://github.com/akinsho/bufferline.nvim.git" },
-  { src = "https://github.com/ThePrimeagen/harpoon.git" },
-  { src = "https://github.com/lewis6991/gitsigns.nvim.git" },
-  { src = "https://github.com/nvim-lua/plenary.nvim.git" },
-  { src = "https://github.com/folke/lazydev.nvim.git" },
-  { src = "https://github.com/folke/trouble.nvim.git" },
-  { src = "https://github.com/folke/which-key.nvim.git" },
-  { src = "https://github.com/folke/snacks.nvim.git" },
-  { src = "https://github.com/neovim/nvim-lspconfig.git" },
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-  { src = "https://github.com/mason-org/mason.nvim.git" },
-  { src = "https://github.com/neovim/nvim-lspconfig.git" },
-  { src = "https://github.com/Saghen/blink.cmp", version = "v1.6.0" },
-  { src = "https://github.com/nvim-lualine/lualine.nvim.git" },
+  { src = 'https://github.com/folke/tokyonight.nvim.git', name = 'tokyonight' },
+  { src = 'https://github.com/nvim-tree/nvim-web-devicons.git' },
+  { src = 'https://github.com/nvim-lua/plenary.nvim.git' },
+  {
+	src = 'https://github.com/ThePrimeagen/harpoon.git',
+	version = 'harpoon2',
+	name = 'harpoon',
+  },
+  { src = 'https://github.com/lewis6991/gitsigns.nvim.git' },
+  { src = 'https://github.com/folke/lazydev.nvim.git' },
+  { src = 'https://github.com/folke/trouble.nvim.git' },
+  { src = 'https://github.com/folke/which-key.nvim.git' },
+  { src = 'https://github.com/folke/snacks.nvim.git' },
+  { src = 'https://github.com/neovim/nvim-lspconfig.git' },
+  { src = 'https://github.com/nvim-treesitter/nvim-treesitter.git' },
+  { src = 'https://github.com/Saghen/blink.cmp.git', version = 'v1.6.0' },
+  { src = 'https://github.com/nvim-lualine/lualine.nvim.git' },
+  { src = 'https://github.com/nvim-mini/mini.pairs.git' },
 })
 
--------------------------------------------------------------------------------
-vim.g.mapleader = " "	-- Space leader
-vim.g.maplocalleader = "\\"
+vim.g.mapleader = ' ' -- Space leader
+vim.g.maplocalleader = '\\'
 
-vim.opt.tabstop = 8
-vim.opt.shiftwidth = 8
-vim.opt.expandtab = false
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = false
 
-vim.cmd("colorscheme tokyonight-night")
+vim.keymap.set('i', 'jj', '<ESC>', { silent = true })
+vim.keymap.set('n', '<leader>e', function()
+  vim.cmd('20Lexplore')
+end, { desc = 'Lexplore' })
+
 vim.g.clipboard = 'tmux'
-vim.opt.mouse = 'nvi'
-vim.opt.number = true
-vim.opt.wrap = true
-vim.opt.relativenumber = true
 vim.g.autoformat = false
-vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25-Cursor,r-cr-o:hor20,t:block-blinkon500-blinkoff500-TermCursor"
-vim.opt.completeopt = "fuzzy,menu,menuone,noinsert,noselect,popup,preview"
-vim.opt.termguicolors = true
--------------------------------------------------------------------------------
+vim.g.netrw_banner = 0
+vim.o.relativenumber = true
+vim.o.colorcolumn = '81'
+vim.o.mouse = 'nvi'
+vim.o.autoindent = true
+vim.o.number = true
+vim.o.cursorline = true
+vim.o.guicursor = 'n-v-c-sm:block-blinkwait1000-blinkon1000-blinkoff1,i-ci-ve:ver25-Cursor,r-cr-o:hor20'
+vim.o.completeopt = 'fuzzy,menu,menuone,noinsert,noselect,popup,preview'
+vim.o.termguicolors = true
+vim.o.scrolloff = 10
+vim.o.ignorecase = true
+vim.o.signcolumn = 'yes:1'
+vim.o.list = true
+vim.o.winborder = 'rounded'
 
-require('plenary').setup {
-}
-
-require('bufferline').setup {
-}
-
---require('harpoon').setup {
---}
+--------------------------------------------- Mini Pairs -----------------------
+require('mini.pairs').setup()
+--------------------------------------------- Harpoon --------------------------
 local harpoon = require('harpoon')
-harpoon:setup()
-vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = 'Harpoon add' })
-vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Harpoon list' })
-vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
-vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+harpoon:setup() -- Required
 
+vim.keymap.set('n', '<C-a>', function()
+  harpoon:list():add()
+end, { desc = 'Harpoon add' })
+vim.keymap.set('n', '<leader>a', function()
+  harpoon:list():add()
+end, { desc = 'Harpoon add' })
 
-require('gitsigns').setup {
-}
+vim.keymap.set('n', '<C-h>', function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end, { desc = 'Harpoon list' })
+vim.keymap.set('n', '<leader>h', function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end, { desc = 'Harpoon list' })
 
-require('lazydev').setup {
-  ft = "lua",
-}
-
-require('lualine').setup {
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  options = {
-    theme = 'ayu_mirage'
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch','diff','diagnostics'},
-    lualine_c = {
-      {'filename', show_filename_only = false, path = 3}
-    },
-    lualine_x = {'lsp_status','encoding','fileformat','filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'},
-  },
-}
-
-require('mason').setup {
-}
-
-require('snacks').setup {
+for i = 1, 9 do
+  local ctrl = string.format('<C-%d>', i)
+  local leader = string.format('<leader>%d', i)
+  vim.keymap.set({ 'n', 'i', 'v' }, ctrl, function()
+    harpoon.list():select(i)
+  end, { desc = '󱡅 Harpoon to ' .. i })
+  vim.keymap.set({ 'n', 'v' }, leader, function()
+    harpoon.list():select(i)
+  end, { desc = '󱡅 Harpoon to ' .. i })
+end
+--------------------------------------------- Git Signs ------------------------
+require('gitsigns').setup()
+--------------------------------------------- LSP ------------------------------
+require('lazydev').setup({
+  ft = 'lua', -- only load on lua files
   opts = {
-    animate = { duration = 10, fps = 144, },
-    bigfile = { enabled = true },
-    dashboard = {
-      enabled = true,
-      preset = {
-	keys = {
-	  { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.picker.smart()" },
-          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
-          { icon = " ", key = "u", desc = "Help poor children in Uganda!", action = ":help iccf" },
-          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-	},
+    library = {
+      { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+    },
+  },
+})
+require('blink.cmp').setup({
+  'saghen/blink.cmp',
+  dependences = {
+    'rafamadriz/friendly-snippets',
+    'folke/lazydev.nvim',
+  },
+  version = '1.6',
+  opts = {
+    keymap = {
+      preset = 'default',
+      ['<C-l>'] = { 'accept' },
+    },
+    fuzzy = {
+      implementation = 'lua',
+      sorts = {
+        'score',
+        'sort_text',
+        'label',
       },
     },
-    explorer = { enabled = true },
+    sources = {
+      default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+      providers = {
+        lazydev = {
+          name = 'LazyDev',
+          module = 'lazydev.integrations.blink',
+          -- make lazydev completions top priority (see `:h blink.cmp`)
+          score_offset = 100,
+        },
+      },
+    },
+  },
+})
+vim.lsp.enable({
+  'lua_ls',
+  'stylua',
+  'clangd',
+  'pyright',
+  'rust_analyzer',
+  'fish_lsp',
+  'bashls',
+})
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'LSP definition' })
+vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, { desc = 'Format code with LSP' })
+--------------------------------------------- Lua Line -------------------------
+require('lualine').setup({
+  options = {
+    theme = 'ayu_mirage',
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = {
+      { 'filename', show_filename_only = false, path = 3 },
+    },
+    lualine_x = { 'lsp_status', 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' },
+  },
+})
+--------------------------------------------- Snacks ---------------------------
+require('snacks').setup({
+    animate = { duration = 10, fps = 144 },
+    bigfile = { enabled = false },
+    dashboard = { enabled = false },
+    explorer = { enabled = false },
     indent = { enabled = true },
-    input = { enabled = true },
+    input = { enabled = false },
     picker = { enabled = true },
-    notifier = { enabled = true , timeout = 10000, },
+    notifier = { enabled = true, timeout = 10000 },
     quickfile = { enabled = true },
     scope = { enabled = true },
     scroll = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
-    terminal = {
-      win = {
-	keys = {
-	  --nav_h = { "<C-h>", term_nav("h"), desc = "Go to Left Window", expr = true, mode = "t" },
-          --nav_j = { "<C-j>", term_nav("j"), desc = "Go to Lower Window", expr = true, mode = "t" },
-          --nav_k = { "<C-k>", term_nav("k"), desc = "Go to Upper Window", expr = true, mode = "t" },
-          --nav_l = { "<C-l>", term_nav("l"), desc = "Go to Right Window", expr = true, mode = "t" },
-	},
-      },
-    },
-  },
-  keys = {
-    { "<leader>ff", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
-    { "<leader>fg", function() Snacks.picker.grep() end, desc = "Grep" },
-    { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
-    { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
-    { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
-    { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
-    { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
-    { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
-    { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
-    { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
-    { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
-  },
-}
-vim.keymap.set('n', "<leader>ff", function() Snacks.picker.smart() end, { desc = "Smart Find Files" })
+    zen = { enabled = false },
+})
+vim.keymap.set('n', '<leader>ff', function()
+    Snacks.picker.smart()
+end, { desc = 'Smart Find Files' })
+vim.keymap.set('n', '<leader>fg', function()
+  Snacks.picker.grep()
+end, { desc = 'Grep' })
+vim.keymap.set('n', 'gd', function()
+  Snacks.picker.lsp_definitions()
+end, { desc = 'Goto Definition' })
+vim.keymap.set('n', 'gD', function()
+  Snacks.picker.lsp_declarations()
+end, { desc = 'Goto Declaration' })
+vim.keymap.set('n', 'gI', function()
+  Snacks.picker.lsp_implementations()
+end, { desc = 'Goto Implementation' })
+vim.keymap.set('n', 'gy', function()
+    Snacks.picker.lsp_type_definitions()
+end, { desc = 'Goto T[y]pe Definition' })
 
-require('tokyonight').setup {
-}
-
-require('nvim-treesitter.configs').setup {
+--------------------------------------------- Colorscheme ----------------------
+require('tokyonight').setup({
+  transparent = true,
+  styles = {
+    sidebars = 'transparent',
+    floats = 'transparent',
+  },
+})
+vim.cmd.colorscheme('tokyonight-night')
+--------------------------------------------- Treesitter -----------------------
+require('nvim-treesitter').setup({
+  lazy = false,
+  branch = 'main',
+  build = ':TSUpdate',
   opts = {
     highlight = { enable = true },
     indent = { enable = true },
     ensure_installed = {
-      "bash",
-      "c",
-      "lua",
-      "luadoc",
-      "luap",
-      "python",
-      "toml",
-      "yaml",
-      "xml",
-      "vim",
-      "vimdoc",
+      'bash',
+      'c',
+      'fish',
+      'lua',
+      'luadoc',
+      'luap',
+      'nu',
+      'python',
+      'rust',
+      'toml',
+      'yaml',
+      'xml',
+      'vim',
+      'vimdoc',
     },
   },
-}
-
-require('trouble').setup {
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {},
+})
+--------------------------------------------- Trouble --------------------------
+require('trouble').setup({
   cmd = "Trouble",
   keys = {
-    { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)", },
-    { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)", },
-    { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)", },
-    { "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP Definitions / references / ... (Trouble)", },
-    { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)", },
-    { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)", },
+    {
+      '<leader>xx',
+      '<cmd>Trouble diagnostics toggle<cr>',
+      desc = 'Diagnostics (Trouble)',
+    },
+    {
+      '<leader>xX',
+      '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+      desc = 'Buffer Diagnostics (Trouble)',
+    },
+    {
+      '<leader>cs',
+      '<cmd>Trouble symbols toggle focus=false<cr>',
+      desc = 'Symbols (Trouble)',
+    },
+    {
+      '<leader>cl',
+      '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+      desc = 'LSP Definitions / references / ... (Trouble)',
+    },
+    {
+      '<leader>xL',
+      '<cmd>Trouble loclist toggle<cr>',
+      desc = 'Location List (Trouble)',
+    },
+    {
+      '<leader>xQ',
+      '<cmd>Trouble qflist toggle<cr>',
+      desc = 'Quickfix List (Trouble)',
+    },
   },
-}
-
-require('which-key').setup {
-  opts = {
-    preset = "helix",
-  },
-}
+})
+--------------------------------------------- Which Key ------------------------
+require('which-key').setup({
+  preset = 'helix',
+})
+------------------------------------------------- Auto Commands ----------------
+-- Highlight on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
