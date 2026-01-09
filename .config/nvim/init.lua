0fd6e82faf77dcc2e677c614fee197e4e57ac817
Tanner Weber <tannerw@pdx.edu>
2026-01-08 18:13:07 -0800

    Nvim: Added git hunks

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -112,6 +112,79 @@ require('lazy').setup({
     --------------------------------------------- Git Signs -------------------
     {
       'lewis6991/gitsigns.nvim',
+      config = function()
+        local gitsigns = require('gitsigns')
+        vim.keymap.set('v', '<leader>hs', function()
+          gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+        end, { desc = 'git [s]tage hunk' })
+        vim.keymap.set('v', '<leader>hr', function()
+          gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+        end, { desc = 'git [r]eset hunk' })
+        vim.keymap.set(
+          'n',
+          '<leader>hs',
+          gitsigns.stage_hunk,
+          { desc = 'git [s]tage hunk' }
+        )
+        vim.keymap.set(
+          'n',
+          '<leader>hr',
+          gitsigns.reset_hunk,
+          { desc = 'git [r]eset hunk' }
+        )
+        vim.keymap.set(
+          'n',
+          '<leader>hS',
+          gitsigns.stage_buffer,
+          { desc = 'git [S]tage buffer' }
+        )
+        vim.keymap.set(
+          'n',
+          '<leader>hu',
+          gitsigns.stage_hunk,
+          { desc = 'git [u]ndo stage hunk' }
+        )
+        vim.keymap.set(
+          'n',
+          '<leader>hR',
+          gitsigns.reset_buffer,
+          { desc = 'git [R]eset buffer' }
+        )
+        vim.keymap.set(
+          'n',
+          '<leader>hp',
+          gitsigns.preview_hunk,
+          { desc = 'git [p]review hunk' }
+        )
+        vim.keymap.set(
+          'n',
+          '<leader>hb',
+          gitsigns.blame_line,
+          { desc = 'git [b]lame line' }
+        )
+        vim.keymap.set(
+          'n',
+          '<leader>hd',
+          gitsigns.diffthis,
+          { desc = 'git [d]iff against index' }
+        )
+        vim.keymap.set('n', '<leader>hD', function()
+          gitsigns.diffthis('@')
+        end, { desc = 'git [D]iff against last commit' })
+        -- Toggles
+        vim.keymap.set(
+          'n',
+          '<leader>tb',
+          gitsigns.toggle_current_line_blame,
+          { desc = '[T]oggle git show [b]lame line' }
+        )
+        vim.keymap.set(
+          'n',
+          '<leader>tD',
+          gitsigns.preview_hunk_inline,
+          { desc = '[T]oggle git show [D]eleted' }
+        )
+      end,
     },
     --------------------------------------------- LSP -------------------------
     {
