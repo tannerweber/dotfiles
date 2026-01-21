7b458ebbcd0d81e98155dbacad1740da2c60baa7
Tanner Weber <tannerweber@proton.me>
2026-01-21 12:19:38 -0800

    chore: conf.nix format

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -22,13 +22,13 @@
   networking = {
     hostName = "nixos"; # Define your hostname.
     networkmanager.enable = true;
-
+    
     # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
     # Configure network proxy if necessary
     # proxy.default = "http://user:password@proxy:port/";
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
+    
     # Open ports in the firewall.
     # firewall.allowedTCPPorts = [ ... ];
     # firewall.allowedUDPPorts = [ ... ];
