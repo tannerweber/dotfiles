c700cc5abedf62cc751e63394fc68e5a44de5139
Tanner Weber <tannerweber@proton.me>
2026-01-22 15:47:44 -0800

    style(nix) used nixfmt

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,14 +1,15 @@
 {
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... } @ inputs:
-  {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      modules = [
-        ./configuration.nix
-      ];
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./configuration.nix
+        ];
+      };
     };
-  };
 }
