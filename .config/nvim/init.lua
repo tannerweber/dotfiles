9eb3b829601c24ca222ca494ce94249765550d43
Tanner Weber <tannerw@pdx.edu>
2026-01-23 14:43:32 -0800

    feat(neovim): mini.ai

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -108,9 +108,14 @@ require('lazy').setup({
   spec = {
     {
       'nvim-mini/mini.pairs',
-      version = '*',
       config = function()
         require('mini.pairs').setup()
+      end,
+    },
+    {
+      'nvim-mini/mini.ai',
+      config = function()
+        require('mini.ai').setup()
       end,
     },
     ---------------------------------------- Database --------------------------
