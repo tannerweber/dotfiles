649d1e883f3fc3ea716e00b5b8b930911f57ee3b
Tanner Weber <tannerw@pdx.edu>
2026-02-21 01:07:16 -0800

    feat(emacs): packages and basic config

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -5,16 +5,72 @@
 (package-initialize)
 (package-refresh-contents)
 
-;; Download Evil
+;; Evil
 (unless (package-installed-p 'evil)
   (package-install 'evil))
-
-;; Enable Evil
 (require 'evil)
 (evil-mode 1)
 
-(custom-set-variables
-'(custom-enabled-themes '(leuven-dark))
-'(package-selected-packages '(evil)))
+;; Rainbow Delimiters
+(unless (package-installed-p 'rainbow-delimiters)
+  (package-install 'rainbow-delimiters))
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 
-(custom-set-faces)
+;; Dirvish
+(unless (package-installed-p 'dirvish)
+  (package-install 'dirvish))
+(dirvish-override-dired-mode)
+
+;; Basic Settings
+(setq default-directory "~/"
+      inhibit-startup-message t
+      initial-scratch-message nil
+      visible-bell t
+      scroll-conservatively 101
+      scroll-margin 10)
+
+(add-to-list 'default-frame-alist
+	     '(font . "Hack Nerd Font"))
+
+(load-theme 'leuven-dark t)
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode 1)
+(global-display-line-numbers-mode 1)
+
+;; Org Mode
+(let* ((variable-tuple
+        (cond ((x-list-fonts "ETBembo")         '(:font "ETBembo"))
+              ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
+              ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
+              ((x-list-fonts "Verdana")         '(:font "Verdana"))
+              ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
+              (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
+       (base-font-color     (face-foreground 'default nil 'default))
+       (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
+
+  (custom-theme-set-faces
+   'user
+   `(org-level-8 ((t (,@headline ,@variable-tuple))))
+   `(org-level-7 ((t (,@headline ,@variable-tuple))))
+   `(org-level-6 ((t (,@headline ,@variable-tuple))))
+   `(org-level-5 ((t (,@headline ,@variable-tuple))))
+   `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
+   `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.2))))
+   `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.3))))
+   `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.5))))
+   `(org-document-title ((t (,@headline ,@variable-tuple :height 1.6 :underline nil))))))
+
+(custom-set-faces
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "red" :height 1.25))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "yellow"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "blue"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "orange"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "green"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "purple"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "lightblue"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "red"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "yellow"))))
+ '(rainbow-delimiters-unmatched-face ((t (:background "blue"))))
+ )
