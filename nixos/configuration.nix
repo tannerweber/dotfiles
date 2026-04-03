8664519a73fc62636270a48e034b050d8478bcce
Tanner Weber <tannerw@pdx.edu>
2026-04-02 19:13:57 -0700

    feat(nix): vim config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -114,13 +114,62 @@
     lazygit.enable = true;
     neovim.enable = true;
     niri.enable = true;
-    vim.enable = true;
-    vim.defaultEditor = true;
     yazi.enable = true;
     zoxide.enable = true;
     git.enable = true;
     starship.enable = true;
     waybar.enable = true;
+  };
+
+  programs.vim = {
+    enable = true;
+    defaultEditor = true;
+    package = (pkgs.vim-full.override { }).customize {
+      name = "vim";
+      vimrcConfig.packages.myplugins = with pkgs.vimPlugins; {
+        start = [
+          vim-nix
+        ];
+        opt = [ ];
+      };
+      vimrcConfig.customRC = ''
+        let g:netrw_banner=0
+        let g:netrw_liststyle=3
+        filetype plugin indent on
+        syntax enable
+
+        set confirm
+        set colorcolumn=81
+        set mouse=a
+        set tabstop=4
+        set shiftwidth=4
+        set number
+        set relativenumber
+        set cursorline
+        set guicursor="n-v-c:block-blinkwait1000-blinkon1000-blinkoff1,i-ci-ve:ver25-Cursor,r-cr-o:hor20"
+        set completeopt="fuzzy,menu,menuone,noinsert,noselect,popup,preview"
+        set termguicolors
+        set scrolloff=10
+        set ignorecase
+        set smartcase
+        set signcolumn="yes"
+        set list
+        set listchars=tab:»\ ,trail:.
+        set splitright
+        set splitbelow
+        set updatetime=250
+        set timeoutlen=300
+
+        set laststatus=2
+        set wrap
+        set hlsearch
+        set statusline=%F\ %m\ %=\ %y\ [%p\%%\ %l\:%c]
+        set wildmenu
+        set wildmode=list:longest
+        colorscheme catppuccin
+        set background=dark
+      '';
+    };
   };
 
   # List packages installed in system profile. To search, run:
