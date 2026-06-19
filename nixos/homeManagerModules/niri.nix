1add59fb927365aa7a098530189db456f02b8b00
Tanner Weber <tannerw@pdx.edu>
2026-06-18 20:32:18 -0700

    feat(nix): rice script links

--- nixos/homeManagerModules/niri.nix
+++ nixos/homeManagerModules/niri.nix
@@ -20,5 +20,7 @@ in
     xdg.configFile."noctalia/colors.json".source = mkSym "${dotDir}/.config/noctalia/colors.json";
     xdg.configFile."noctalia/plugins.json".source = mkSym "${dotDir}/.config/noctalia/plugins.json";
     xdg.configFile."noctalia/settings.json".source = mkSym "${dotDir}/.config/noctalia/settings.json";
+    home.file.".rice/powermenu.sh".source = mkSym "${dotDir}/.rice/powermenu.sh";
+    home.file.".rice/get_random_wallpaper.sh".source = mkSym "${dotDir}/.rice/get_random_wallpaper.sh";
   };
 }
