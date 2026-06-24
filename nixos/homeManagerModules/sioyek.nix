d2f6b241d8cff31213f209f4d56210e86bb0a421
Tanner Weber <tannerw@pdx.edu>
2026-06-23 18:58:26 -0700

    feat(nix): sioyek xwayland force

--- nixos/homeManagerModules/sioyek.nix
+++ nixos/homeManagerModules/sioyek.nix
@@ -11,13 +11,36 @@
   };
 
   config = lib.mkIf config.myModSioyek.enable {
-    programs.sioyek.enable = true;
+    programs.sioyek = {
+      enable = true;
+      package = (
+        pkgs.writeShellScriptBin "sioyek" ''
+          exec env QT_QPA_PLATFORM=xcb ${pkgs.sioyek}/bin/sioyek "$@"
+        ''
+      );
+      config = {
+        background_color = "0.5 0.5 0.5";
+        page_separator_width = "10";
+        page_separator_color = "0.5 0.5 0.5";
+      };
+    };
 
     xdg.mimeApps = {
       enable = true;
       defaultApplications = {
         "application/pdf" = "sioyek.desktop";
       };
+    };
+
+    xdg.desktopEntries.sioyek = {
+      name = "Sioyek";
+      exec = "env QT_QPA_PLATFORM=xcb sioyek %U";
+      icon = "sioyek";
+      terminal = false;
+      categories = [
+        "Office"
+        "Viewer"
+      ];
     };
   };
 }
