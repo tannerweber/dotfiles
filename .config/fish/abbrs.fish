dcd518ea7aa77a5345edbfbfdb053fac4d3e1f88
Tanner Weber <tannerw@pdx.edu>
2026-08-08 22:24:02 -0700

    fish: git abbrs

--- .config/fish/abbrs.fish
+++ .config/fish/abbrs.fish
@@ -1,17 +1,21 @@
-abbr --add ... cd .. \&\& cd ..
+abbr --add ... "cd .. && cd .."
 abbr --add la "eza -al --icons --group"
 abbr --add d "eza -al --icons --group"
-abbr --add c clear
-abbr --add gs git status
-abbr --add gd git diff
-abbr --add gds git diff --staged
-abbr --add gl git log
-abbr --add gc git commit -m
-abbr --add gb git branch
-abbr --add grv git remote -v
-abbr --add gad git add .
-abbr --add gau git add -u
-abbr --add gaa git add -A
-abbr --add gpom git push origin main
-abbr --add n nvim
+abbr --add c "clear"
+abbr --add gs "git status"
+abbr --add gd "git diff"
+abbr --add gds "git diff --staged"
+abbr --add gl "git log"
+abbr --add gco "git commit -m"
+abbr --add gb "git branch"
+abbr --add gc "git checkout"
+abbr --add gcm "git checkout main"
+abbr --add grv "git remote -v"
+abbr --add gad "git add ."
+abbr --add gau "git add -u"
+abbr --add gaa "git add -A"
+abbr --add gwa "git worktree add"
+abbr --add gwr "git worktree remove"
+abbr --add gpom "git push origin main"
+abbr --add n "nvim"
 abbr --add nsd "nix store diff-closures /nix/var/nix/profiles/system-"
